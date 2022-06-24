a4a6a8a3a21dec0d782e9c9caac8bc35e653c927
Rustam Aliiev <r.aliiev@mobidev.biz>
2022-06-24 19:29:52 +0300

    text under arc is added

--- app/src/main/java/com/example/digitalbankingapp/view/screens/TransactionsScreen.kt
+++ app/src/main/java/com/example/digitalbankingapp/view/screens/TransactionsScreen.kt
@@ -1,18 +1,16 @@
 package com.example.digitalbankingapp.view.screens
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.digitalbankingapp.TransactionsAppBar
@@ -26,9 +24,16 @@ fun TransactionsScreen(
     Scaffold(
         topBar = { TransactionsAppBar(navController) }
     ) {
-        TransactionsArc(
-            percentage = 0.8f,
-        )
+        Box(modifier = Modifier.padding(16.dp)) {
+
+            TransactionsArc(
+                percentage = 0.8f,
+            )
+            Column(modifier = Modifier.align(Alignment.TopCenter)) {
+                Text(text = "Balance")
+                Text(text = "15 000 usd")
+            }
+        }
     }
 }
 
@@ -39,24 +44,27 @@ fun TransactionsArc(
 ) {
     Canvas(
         modifier = modifier
-            .padding(16.dp)
+//            .padding(16.dp)
             .height(380.dp)
             .fillMaxWidth()
     )
     {
         val strokeWidth = 35.dp
         val sizeArc = size / 1.2f
+        val topLeft = Offset(
+            x = (size.width - sizeArc.width) / 2f,
+            y = (size.height - sizeArc.height) / 2f
+        )
+        val stroke = Stroke(strokeWidth.toPx(), cap = StrokeCap.Butt)
+
         //eCommerce line
         drawArc(
             color = ArcCommerceColor,
             startAngle = -180f,
             sweepAngle = 180f,
-            topLeft = Offset(
-                x = (size.width - sizeArc.width) / 2f,
-                y = (size.height - sizeArc.height) / 2f
-            ),
+            topLeft = topLeft,
             useCenter = false,
-            style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Butt),
+            style = stroke,
             size = sizeArc
         )
         //ePayment line
@@ -64,15 +72,12 @@ fun TransactionsArc(
             color = ArcPaymentColor,
             startAngle = -180f,
             sweepAngle = 180f * percentage,
-            topLeft = Offset(
-                x = (size.width - sizeArc.width) / 2f,
-                y = (size.height - sizeArc.height) / 2f
-            ),
+            topLeft = topLeft,
             useCenter = false,
-            style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Butt),
+            style = stroke,
             size = sizeArc
         )
-        
+
     }
 }
 
